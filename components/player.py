5731ed292955c164d0d99b6125903ff5cd2c707b
Leanryl <jesus.romero007.jr@gmail.com>
2020-12-01 19:29:15 -0400

    Clase 6
    Clase 6

--- components/player.py
+++ components/player.py
@@ -5,21 +5,31 @@ from utils.constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH
 )
+from components.bullet import Bullet
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         pygame.sprite.Sprite.__init__(self)
+        self.game = game
         self.image = pygame.Surface((50, 50))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = SCREEN_WIDTH/2
-        self.rect.centery = SCREEN_HEIGHT/2
+        self.rect.centery = SCREEN_HEIGHT - 10
 
     def update(self):
+        self.movement_on_x = 10
         key = pygame.key.get_pressed()
         if key[pygame.K_RIGHT]:
             if self.rect.right < SCREEN_WIDTH:
                 self.rect.centerx += 5
+
         if key[pygame.K_LEFT]:
             if self.rect.left < SCREEN_WIDTH and self.rect.left > 0:
                 self.rect.centerx -= 5
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        self.game.all_sprites.add(bullet)
+        self.bullets = pygame.sprite.Group()
+        self.bullets.add(bullet)
